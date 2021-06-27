313f350b8e8616db258cabc8de1457e14bc6ea10
mukesh raja <79893303+mukeshraja121@users.noreply.github.com>
2021-06-27 21:33:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+//@ts-ignore
+import Innfo from "./Innfo";
+import Footer from "./Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Innfo />
+      <Innfo />
+      <Innfo />
+      <Innfo />
+      <Innfo />
+      
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Innfo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Innfo(){
+  return(
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic web dec React Js Bootcamp</p>
+    </div>
+  );
+}
+export default Innfo;
